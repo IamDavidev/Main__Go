55d2c34338025ab6deebe8b2df87c2955af02178
L-David-26 <davidlez2mgk@gmail.com>
2021-09-23 22:29:51 -0500

    initial proyect whit Go

---
+++ VariblesGo/main.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+var num int 
+var text string
+var status bool
+
+func main() {
+	valueUno, valueDos, valueTres := 1 , "valor dos tomado en go",true
+
+	fmt.Println(valueUno)
+	fmt.Println(valueDos)
+	fmt.Println(valueTres)
+}
