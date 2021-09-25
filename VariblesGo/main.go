e82188a02f0382c8e77d946451cd714ce260866f
L-David-26 <davidlez2mgk@gmail.com>
2021-09-24 21:58:58 -0500

    varibles y conversiones de typado

--- VariblesGo/main.go
+++ VariblesGo/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 //para poder exportar varibles o funciones  tienen que empezar con Mayuscula
 // => ejmplo
@@ -13,13 +16,21 @@ func FuncionExportando (){
 }
 // ..
 var num int = 10 
-var text string = "hola soy de tipo texto"
+var text string 
 var status bool = false || true 
 
 func main() {
+	/*
 	valueUno, valueDos, valueTres := 1 , "valor dos tomado en go",true
 	fmt.Println(valueUno)
 	fmt.Println(valueDos)
 	fmt.Println(valueTres)
+	*/
+	Variables()
 }
 
+func Variables () {
+ 	var valor int64 = 1369
+	var text = strconv.Itoa(int(valor))
+	fmt.Println(text)
+} 
