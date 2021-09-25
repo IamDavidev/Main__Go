eeed6df3a6f5771b03d0bf03f08c74143f415be1
L-David-26 <davidlez2mgk@gmail.com>
2021-09-24 21:34:39 -0500

    variables

--- VariblesGo/main.go
+++ VariblesGo/main.go
@@ -1,15 +1,25 @@
-package main 
+package main
 
 import "fmt"
 
-var num int 
-var text string
-var status bool
+//para poder exportar varibles o funciones  tienen que empezar con Mayuscula
+// => ejmplo
+var Export string = "exportvariable exportable"
+func FuncionExportando (){
+	fmt.Println(Export)
+
+
+
+}
+// ..
+var num int = 10 
+var text string = "hola soy de tipo texto"
+var status bool = false || true 
 
 func main() {
 	valueUno, valueDos, valueTres := 1 , "valor dos tomado en go",true
-
 	fmt.Println(valueUno)
 	fmt.Println(valueDos)
 	fmt.Println(valueTres)
 }
+
